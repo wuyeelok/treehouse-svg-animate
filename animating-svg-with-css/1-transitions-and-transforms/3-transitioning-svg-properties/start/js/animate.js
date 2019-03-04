843613e0ee8789db37ebf4a6eb6f779db890eca5
wuyeelok <wuyeelok@gmail.com>
2019-03-04 18:00:03 +0800

    Add hover effect to the rest icon

--- animating-svg-with-css/1-transitions-and-transforms/3-transitioning-svg-properties/start/js/animate.js
+++ animating-svg-with-css/1-transitions-and-transforms/3-transitioning-svg-properties/start/js/animate.js
@@ -3,10 +3,31 @@ const gearBGEle = document.querySelector('.gear-bg')
 const gearBGHoverAnimation = new TimelineMax({paused: true})
 gearBGHoverAnimation.to(gearBGEle, .3, {ease: Power1.easeOut, fill: '#3881cc'})
 
-gearBGEle.addEventListener('mouseenter', () => {
-    gearBGHoverAnimation.play()
-})
 
-gearBGEle.addEventListener('mouseleave', () => {
-    gearBGHoverAnimation.reverse()
-})
+const hammerBGEle = document.querySelector('.hammer-bg')
+
+const hammerBGHoverAnimation = new TimelineMax({paused: true})
+hammerBGHoverAnimation.to(hammerBGEle, .3, {ease: Power1.easeOut, fill: '#feae6e'})
+
+
+const heartBGEle = document.querySelector('.heart-bg')
+
+const heartBGHoverAnimation = new TimelineMax({paused: true})
+heartBGHoverAnimation.to(heartBGEle, .3, {ease: Power1.easeOut, fill: '#ff5e5e'})
+
+
+const iconHoverAnimateBind = (ele, animation) => {
+    ele.addEventListener('mouseenter', () => {
+        animation.play()
+    })
+
+    ele.addEventListener('mouseleave', () => {
+        animation.reverse()
+    })
+}
+
+iconHoverAnimateBind(gearBGEle, gearBGHoverAnimation)
+
+iconHoverAnimateBind(hammerBGEle, hammerBGHoverAnimation)
+
+iconHoverAnimateBind(heartBGEle, heartBGHoverAnimation)
